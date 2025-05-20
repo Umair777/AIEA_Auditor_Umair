7bcb56ee223f37c2abee6b82797e81363bb97469
Umair <ashrafumair21@gmail.com>
2025-05-19 20:00:32 -0700

    create prolog facts and rules

--- Task4/simpsons.pl
+++ Task4/simpsons.pl
@@ -1,16 +1,25 @@
-% ----- 10 facts -----
-parent(homer, bart).
-parent(homer, lisa).
-parent(homer, maggie).
-parent(marge, bart).
-parent(marge, lisa).
-parent(marge, maggie).
-parent(abe,  homer).
+:- discontiguous parent/2.
+
+
+% ----- 5 parent/2 facts -----
+parent(abe, homer).
 parent(mona, homer).
 parent(clancy, marge).
 parent(jacqueline, marge).
+parent(herb, zeke).
 
-% ----- 1 rule -----
-grandparent(GP, GC) :-      % GP is grandparent of GC
-    parent(GP, P),          % if GP is parent of P
-    parent(P, GC).          % and P is parent of GC
+% ----- 5 parents/3 facts -----
+parents(homer, marge, bart).
+parents(homer, marge, lisa).
+parents(homer, marge, maggie).
+parents(herb, clara, jill).
+parents(bart, mary, jake).
+
+% A person is a parent if they are one of the two in parents/3
+parent(P, C) :- parents(P, _, C).   % P is the father
+parent(P, C) :- parents(_, P, C).   % P is the mother
+
+% A grandparent is a parent of a parent
+grandparent(GP, GC) :-
+    parent(GP, P),
+    parent(P, GC).
